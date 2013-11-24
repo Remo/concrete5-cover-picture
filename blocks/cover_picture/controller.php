24382c8e7bbfa06878e2b9ed4965b0a7b4dcf47c
Remo <remo.laubacher@gmail.com>
2013-11-24 21:36:42 +0100

    block name and description changed

--- blocks/cover_picture/controller.php
+++ blocks/cover_picture/controller.php
@@ -7,6 +7,14 @@ class CoverPictureBlockController extends Concrete5_Controller_Block_Content {
     protected $btTable = 'btCoverPicture';
     protected $btInterfaceHeight = "645";
 
+    public function getBlockTypeDescription() {
+        return t("Cover picture where you can add additional text that shows up when you hover the picture.");
+    }
+
+    public function getBlockTypeName() {
+        return t("Cover Picture");
+    }
+
     public function save($args) {
         $args['content'] = $this->translateTo($args['content']);
         $args['fID'] = intval($args['fID']);
