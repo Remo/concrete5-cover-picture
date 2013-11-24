6a651eeb6c2599289e3aa8bc12094c36d0c588e3
Remo <remo.laubacher@gmail.com>
2013-11-24 21:31:14 +0100

    labels added

--- blocks/cover_picture/add.php
+++ blocks/cover_picture/add.php
@@ -4,9 +4,15 @@ defined('C5_EXECUTE') or die('Access Denied.');
 $bt->inc('editor_init.php');
 ?>
 
+<h2><?php echo t('Cover Picture')?></h2>
+<?php
+echo $this->inc('file_selector.php');
+?>
+
+<h2><?php echo t('Text')?></h2>
+
 <div style="text-align: center">
     <textarea id="ccm-content-<?php echo $a->getAreaID() ?>" class="advancedEditor ccm-advanced-editor" name="content" style="width: 580px; height: 380px"></textarea>
 </div>
 
-<?php
-echo $this->inc('file_selector.php');
+
--- blocks/cover_picture/controller.php
+++ blocks/cover_picture/controller.php
@@ -5,6 +5,7 @@ defined('C5_EXECUTE') or die('Access Denied.');
 class CoverPictureBlockController extends Concrete5_Controller_Block_Content {
 
     protected $btTable = 'btCoverPicture';
+    protected $btInterfaceHeight = "640";
 
     public function save($args) {
         $args['content'] = $this->translateTo($args['content']);
