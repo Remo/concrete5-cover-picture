4b852320f0add68c1bfdd57b3ea81110a83f0483
Remo Laubacher <remo.laubacher@gmail.com>
2013-12-03 14:00:33 +0100

    text centered template added

---
+++ blocks/cover_picture/templates/text_centered/view.php
@@ -0,0 +1,23 @@
+<?php
+defined('C5_EXECUTE') or die('Access Denied.');
+
+$content = $controller->getContent();
+$fileObject = File::getByID($fID);
+
+if (!is_object($fileObject) || $fileObject->isError()) {
+    echo '<div style="color: red; font-weight: bold;">' . t('No picture selected') . '</div>';
+    return;    
+}
+
+$width = $fileObject->getAttribute('width');
+$height = $fileObject->getAttribute('height');
+?>
+
+<div class="cover-picture-text-centered" style="width: <?php echo $width?>px; height: <?php echo $height?>px; overflow: hidden;">
+    <img src="<?php echo $fileObject->getURL() ?>" alt=""/>
+    <div class="cover-picture-text">
+        <div class="cover-picture-text-inner">
+            <?php echo $content ?>
+        </div>    
+    </div>    
+</div>
